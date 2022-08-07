d57cf756d6651191f3e69675e61104d08e1eab88
Lucia <luciam.abellan@gmail.com>
2022-08-07 13:55:17 +0200

    [ex_03] finished

---
+++ exercise_3/exercise_3/ex3.java
@@ -0,0 +1,29 @@
+package exercise_3;
+
+import java.util.Scanner;
+
+public class ex3 {
+
+    public static void main (String[] args){
+        int max = 20;
+        int num;
+        String tecla;
+        Scanner sc = new Scanner(System.in);
+      
+        
+        System.out.println("Write a number from 1-20: ");
+        num = sc.nextInt();
+        
+        
+        for(int i=1; i<=num; i++){
+            for(int j=1; j<=max; j++){
+                System.out.println(j+" x "+i+" = "+j*i);
+            }
+        }
+       
+           
+        
+        
+    }
+    
+}
